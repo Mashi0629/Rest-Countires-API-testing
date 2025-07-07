1d023484973e9405eb7313412383404da446123f
Mashi0629 <poboshiniomashi@gmail.com>
2025-07-07 21:22:13 +0530

    add displayCountries function
    -

--- js/restapi.js
+++ js/restapi.js
@@ -1,22 +1,27 @@
 const loadCountryAPI = () => {
     fetch('https://restcountries.com/v3.1/all?fields=name,capital,flags,population,languages')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            displayCountries(data); 
+        })
         .catch(error => console.error("Error fetching data:", error));
-}
+};
+
 
 const displayCountries = countries =>{
     console.log(countries);
     const countriesHTML = countries.map(country => getCountry(country))
     //displaying div to html
     const container =  document.getElementById('countries');
-    container.innerHTML = countriesHTML;
+    container.innerHTML = countriesHTML.join(' ');
+
 }
 
 const getCountry = (country) =>{
     console.log(country)
     return `
-    <div>
+    <div class="country-div">
     <h2>${country.name.common} </h2>
     </div>
     `
