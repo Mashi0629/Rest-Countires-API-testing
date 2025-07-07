cb5bd05507db499a8dd1b0b5f9e1d28eaba69c8b
Mashi0629 <poboshiniomashi@gmail.com>
2025-07-07 20:00:14 +0530

    <div> not converting to html
    still working on it

--- js/restapi.js
+++ js/restapi.js
@@ -5,4 +5,20 @@ const loadCountryAPI = () => {
         .catch(error => console.error("Error fetching data:", error));
 }
 
+const displayCountries = countries =>{
+    console.log(countries);
+    const countriesHTML = countries.map(country => getCountry(country))
+    //displaying div to html
+    const container =  document.getElementById('countries');
+    container.innerHTML = countriesHTML;
+}
+
+const getCountry = (country) =>{
+    console.log(country)
+    return `
+    <div>
+    <h2>${country.name.common} </h2>
+    </div>
+    `
+}
 loadCountryAPI();
